da5455bdf3e58f22a0e92ef583481c0251cea70f
w32zhong <clock126@126.com>
2026-02-21 02:04:24 -0500

    gateway fix: lift query_params definition.

--- app/gateway/rewrite.lua
+++ app/gateway/rewrite.lua
@@ -7,6 +7,7 @@ local route = ngx.var.service_route
 ngx.req.clear_header("X-Remote-User")
 
 local modified_uri = ngx.var.modified_uri
+local query_params = ngx.var.is_args .. (ngx.var.args or '')
 local full_req_uri = ngx.var.request_uri
 
 -- Anything /non_root will be redirected to /non_root/
@@ -120,6 +121,5 @@ end
 --------------------------
 --- Print Passed Route ---
 --------------------------
-local query_params = ngx.var.is_args .. (ngx.var.args or '')
 print('[route] pass: ', full_req_uri, ' ==> ',
     ngx.var.service_addr, ngx.var.modified_uri, query_params)
