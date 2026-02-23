94eddf55750ca1fc5cb2f922dcb5d1e21c0e193c
w32zhong <clock126@126.com>
2026-02-22 23:50:01 -0500

    minor update

--- app/ui_login/vite.config.js
+++ app/ui_login/vite.config.js
@@ -5,12 +5,6 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   base: './',
   plugins: [vue()],
-	/* Vite dev server runs on 5173, we need this proxy to get around CORS */
-  server: {
-    proxy: {
-      '/oauth2': 'http://localhost:8080'
-    }
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
