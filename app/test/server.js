dc64a1740a7a297628f12c95ab1bdd8fcfa3d3b6
w32zhong <clock126@126.com>
2026-02-23 15:37:48 -0500

    test oauth2: allows Passport to trust the headers for local dev.

--- app/test/server.js
+++ app/test/server.js
@@ -24,7 +24,8 @@ app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID || 'dummy_id',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'dummy_secret',
-    callbackURL: "/auth/google/callback"
+    callbackURL: "/auth/google/callback",
+    proxy: true // Allows Passport to trust the headers from your tunnel/proxy
   },
   (accessToken, refreshToken, profile, done) => {
     // This verify callback is called after Phase 2 (Code Exchange) is successful.
@@ -50,6 +51,13 @@ app.get('/auth/google',
  * 4. Calls the Strategy's verify callback and populates 'req.user' with the result.
  */
 app.get('/auth/google/callback',
+    // When the browser hits the callback, it doesn't have a token yet, it only has
+    // a temporary "claim check" called a "code".
+    // If Google sent the actual access token in this callback URL instead of a temporary
+    // code, the token key would be dangerously exposed (e.g., by an open Wi-Fi).
+    // Instead, if a hacker gets this "code", they still can't do anything with it.
+    // To exchange that code for an access token, they must also have your Google Oauth2
+    // Client credentials and that secret lives exclusively on your backend server.
     passport.authenticate('google', { failureRedirect: '/', session: false }),
     (req, res) => {
         // req.user is now populated by the second passport.authenticate() call.
@@ -66,7 +74,9 @@ app.get('/auth/google/callback',
         // Set Cookie: Send the JWT back to the browser.
         res.cookie('jwt', token, {
             httpOnly: false, // Set to false so frontend JS can read it for this learning code
-            secure: true, // The browser will only send the cookie if the connection is HTTPS
+            secure: true, // browser will only send the cookie if it is HTTPS, this is true even
+                          // in dev mode because most OAuth2 providers requires setting an HTTPS
+                          // Authorized redirect URI to even test the OAuth2 process.
             maxAge: 3600 * 1000 // 1hr in Express.js, maxAge is in milliseconds
         });
 
